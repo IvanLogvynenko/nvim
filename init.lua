73a47b29cd27c977baeeff5700e2440d94c659ff
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2025-03-17 16:50:57 +0100

    some raw config

---
+++ init.lua
@@ -0,0 +1,18 @@
+require("config.lazy")
+
+vim.o.showmatch = true
+vim.o.ignorecase = true
+vim.o.mouse = v
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.tabstop = 4
+vim.o.softtabstop=4
+vim.o.autoindent = true
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.number = true
+vim.o.syntax = on
+vim.o.wildmode = longest,list 
+vim.o.mouse = a
+vim.o.clipboard = unnamedplus
+vim.o.cursorline = true
---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"baliestri/aura-theme", 
+		lazy = false,
+		prioryty = 1000,
+		config = function(plugin) 
+			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+			vim.cmd([[colorscheme aura-dark]])
+		end,
+	},
+	{ "folke/todo-comments.nvim", opts = {} }
+}
