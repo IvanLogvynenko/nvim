d7f4cb95ebeb1b13720b8df57596da0fbe281841
Ivan Logvynenko <ivanlogvynenko2112@gmail.com>
2025-04-20 14:37:21 +0200

    setup right nvim config

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        { "3rd/image.nvim", opts = {}},
+    },
+    config = function()
+        vim.keymap.set('n', '<leader>r', ':Neotree filesystem reveal right<CR>')
+    end,
+    lazy = false,
+}
